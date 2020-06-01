328cacefc42e1ec0f650ee8e8b02167013a88350
Mike Rossetti <s2t2@users.noreply.github.com>
2020-06-01 14:22:07 -0400

    Prepare to distribute our package

--- my_lambdata/hello.py
+++ my_lambdata/hello.py
@@ -1,9 +1,12 @@
 
-
-#from my_mod import enlarge
+from pandas import DataFrame
 
 from my_lambdata.my_mod import enlarge
 
 print("HELLO")
 
 print(enlarge(8))
+
+
+df = DataFrame({"state": ["CT", "CO", "CA", "TX"]})
+print(df.head())
